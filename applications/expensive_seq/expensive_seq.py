3f406841789808611815d76a2b32cbd3a3580a95
Brett Karr <b.karr@polyclip.us>
2020-09-09 21:55:05 -0500

    lookup, wordcount, and expensive_seq completed

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,14 +1,28 @@
 # Your code here
 
+def exps(x, y, z):
+    # the 'ugly' way...
+    if x <= 0:
+        return y+z
+    else:
+        res = exps(x-1, y+1, z) + exps(x-2, y+2, z*2) + exps(x-3, y+3, z*3)
+        return res
 
+cache = {}
 def expensive_seq(x, y, z):
     # Your code here
+    if x <=0:
+        return y + z
+    if (x, y, z) not in cache:
+        cache[x, y, z] = expensive_seq(x-1, y+1, z) + expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
 
-
+    return cache[(x, y, z)]
 
 if __name__ == "__main__":
     for i in range(10):
+        # x = exps(i*2, i*3, i*4)
         x = expensive_seq(i*2, i*3, i*4)
         print(f"{i*2} {i*3} {i*4} = {x}")
 
+    # print(exps(150, 400, 800))
     print(expensive_seq(150, 400, 800))
--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,39 @@
+import string
+ascii_letters = set(map(ord, string.ascii_letters))
+non_letters = ''.join(chr(i) for i in range(256) if i not in ascii_letters)
+
+# cache = {}
+
 def word_count(s):
     # Your code here
+    cache = {}
+    lower = s.lower()
+    for symbol in non_letters:
+        if symbol == "'":
+            pass
+        else:
+            lower = lower.replace(symbol, "\n")
+
+    for each in lower.split():
+        # print('each: ', each)
+        if each == "":
+            continue
+        if each not in cache:
+            cache[each] = 1
+        else:
+            cache[each] += 1
+
+    return cache
+
+# def sort_by_val(s):
+#     counts_dict = letter_counts(s)  # taken from lecture
+#     ## sort by value
+#     counts_list = list(counts_dict.items())
+#     # sort by value, AND in descending order
+#     counts_list.sort(reverse = True, key = lambda x: x(1))
+#
+#     for pair in counts_list:
+#         print(f'count: {pair{1}} letter: {pair{0}')
 
 
 
